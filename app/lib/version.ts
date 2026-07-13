13f4bc9ac33eaeefbd647db01075dc27d59bfd43
Sam <sam@tealstreet.io>
2026-07-13 12:49:02 +0800

    chore: release Build 5 (testflight)

--- app/lib/version.ts
+++ app/lib/version.ts
@@ -3,9 +3,9 @@
 
 export const APP_VERSION = {
   version: "1.0.0", // Marketing version for app stores
-  buildNumber: 4, // Native build number (increments only for native builds)
-  otaVersion: 6, // OTA update version (increments for JS updates)
-  timestamp: "2026-06-29T13:37:59Z", // Last update timestamp
+  buildNumber: 5, // Native build number (increments only for native builds)
+  otaVersion: 0, // OTA update version (increments for JS updates)
+  timestamp: "2026-07-13T04:49:02Z", // Last update timestamp
   channel: "testflight", // Release channel
 };
 
